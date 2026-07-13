619d1cd08cae2d01627929add05764652ecc1951
Andrew Jones <arejones@gmail.com>
2026-07-13 15:50:51 +0100

    Initial Commit

---
+++ MeasureShotTests/MeasureShotTests.swift
@@ -0,0 +1,19 @@
+//
+//  MeasureShotTests.swift
+//  MeasureShotTests
+//
+//  Created by Andrew Jones on 7/13/26.
+//
+
+import Testing
+@testable import MeasureShot
+
+struct MeasureShotTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
